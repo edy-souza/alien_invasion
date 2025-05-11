4d48dc6d8e5497fa416971a5d99cd58c1af32364
Edy Souza <edysouza363@gmail.com>
2025-05-11 15:22:00 -0300

    Disparando balas e Armazenando projéteis em um grupo

--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings:
         
         # Configurações da nave
         self.ship_speed = 1.5 
+        
+        # Configurações do projétil
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
